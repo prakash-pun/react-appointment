2ff2e76d30389dbfa04412fe49d1009e76c12397
prakash-pun7 <punbishal777@gmail.com>
2021-06-06 12:35:00 +0545

    sorting and interface

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
 
   let [appointmentList, setAppointmentList] = useState([]);
   let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState("asc");
 
   const filteredAppointments = appointmentList.filter(
     item => {
@@ -18,7 +20,13 @@ function App() {
         item.aptNotes.toLowerCase().includes(query.toLowerCase())
       );
     }
-  )
+  ).sort((a, b) => {
+    let order = (orderBy === 'asc') ? 1 : -1;
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+        ? -1 * order : 1 * order
+    )
+  })
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -37,7 +45,12 @@ function App() {
       <h1 className="text-5xl mb-3"><BiCalendar className="inline-block text-red-600 align-top" />Prakash Pun</h1>
       <AddAppointment />
       <Search query={query}
-        onQueryChage={myQuery => setQuery(myQuery)} />
+        onQueryChage={myQuery => setQuery(myQuery)} 
+        orderBy={orderBy}
+        onOrderByChange={mySort => setOrderBy(mySort)}
+        sortBy={sortBy}
+        onSortByChange = {mySort => setSortBy(mySort)}
+      />
 
       <ul className="divide-y divide-gray-200">
         {filteredAppointments
